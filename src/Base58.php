793f73ebfbb385ae5049f5c2f48ce24a0328fed5
recanman <29310982+recanman@users.noreply.github.com>
2024-05-15 23:29:14 -0700

    feat: BigInt, use it in base58

--- src/Base58.php
+++ src/Base58.php
@@ -4,27 +4,26 @@ declare(strict_types=1);
 
 namespace MoneroIntegrations\MoneroCrypto;
 
-use \Exception;
+use Exception;
 
 class Base58
 {
-    const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
-    const ALPHABET_SIZE = "58";
-    const BLOCK_SIZE = 8;
-    const ENCODED_BLOCK_SIZES = [0, 2, 3, 5, 6, 7, 9, 10, 11];
-    const FULL_ENCODED_BLOCK_SIZE = 11;
+    public const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+    public const ALPHABET_SIZE = 58;
+    public const BLOCK_SIZE = 8;
+    public const ENCODED_BLOCK_SIZES = [0, 2, 3, 5, 6, 7, 9, 10, 11];
+    public const FULL_ENCODED_BLOCK_SIZE = 11;
 
     /**
      * Converts an array of unsigned 8-bit big-endian integers to a 64-bit unsigned integer.
      */
-    private static function uint8beTo64(array $data): string
+    private static function uint8beTo64(array $data): BigInteger
     {
-        $res = '0';
+        $res = new BigInteger(0);
         $size = count($data);
 
         for ($i = 0; $i < $size; $i++) {
-            $res = bcmul($res, '256');
-            $res = bcadd($res, (string)ord($data[$i]));
+            $res = $res->mul(256)->add(ord($data[$i]));
         }
 
         return $res;
@@ -33,13 +32,13 @@ class Base58
     /**
      * Converts a decimal string to a hexadecimal string.
      */
-    private static function decToHex(string $dec): string
+    private static function decToHex(BigInteger $dec): string
     {
         $res = '';
-        while (bccomp($dec, '0') > 0) {
-            $remainder = bcmod($dec, '16');
-            $dec = bcdiv($dec, '16');
-            $res = dechex((int)$remainder) . $res;
+        while (!$dec->equals(0)) {
+            $remainder = $dec->mod(16);
+            $dec = $dec->div(16);
+            $res = dechex((int)$remainder->toDec()).$res;
         }
 
         return $res;
@@ -53,16 +52,16 @@ class Base58
         $length = count($data);
 
         if ($length < 1 || $length > self::FULL_ENCODED_BLOCK_SIZE) {
-            throw new Exception("Invalid block length: " . $length);
+            throw new Exception("Invalid block length: ". $length);
         }
 
         $num = self::uint8beTo64($data);
         $i = self::ENCODED_BLOCK_SIZES[$length] - 1;
 
-        while ($num > 0) {
-            $remainder = bcmod($num, self::ALPHABET_SIZE);
-            $num = bcdiv($num, self::ALPHABET_SIZE);
-            $res[$res_offset + $i] = self::ALPHABET[$remainder];
+        while (!$num->equals(0)) {
+            $remainder = $num->mod(self::ALPHABET_SIZE);
+            $num = $num->div(self::ALPHABET_SIZE);
+            $res[$res_offset + $i] = self::ALPHABET[$remainder->toDec()];
             $i--;
         }
 
@@ -106,21 +105,20 @@ class Base58
         $length = count($data);
 
         if ($length < 1 || $length > self::FULL_ENCODED_BLOCK_SIZE) {
-            throw new Exception("Invalid block length: " . $length);
+            throw new Exception("Invalid block length: ". $length);
         }
 
-        $num = '0';
+        $num = new BigInteger(0);
 
         for ($i = 0; $i < $length; $i++) {
             $char = $data[$i];
             $char_value = strpos(self::ALPHABET, $char);
 
             if ($char_value === false) {
-                throw new Exception("Invalid character: " . $char);
+                throw new Exception("Invalid character: ". $char);
             }
 
-            $num = bcmul($num, self::ALPHABET_SIZE);
-            $num = bcadd($num, (string)$char_value);
+            $num = $num->mul(self::ALPHABET_SIZE)->add($char_value);
         }
 
         $res = array_merge($res, str_split(pack('H*', self::decToHex($num))));
